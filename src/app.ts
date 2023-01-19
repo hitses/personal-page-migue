e706afbfb2a957c9849f1af9b49ce39ab01b4575
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-01-19 10:48:42 +0100

    Add CORS & new scripts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -11,6 +12,7 @@ const app = express()
 app.set('port', process.env.PORT)
 
 // Middlewares
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
